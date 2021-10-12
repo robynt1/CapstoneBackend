f45990c2eb20ace089ec2dd82e017281bf72f066
Lukasz <wyspianski.lukasz@gmail.com>
2021-10-12 13:55:44 +0000

    Inject the repository layer into the service

--- src/main/java/com/example/hobbitron/service/HobbitService.java
+++ src/main/java/com/example/hobbitron/service/HobbitService.java
@@ -1,17 +1,21 @@
 package com.example.hobbitron.service;
 
 import com.example.hobbitron.model.Hobbit;
+import com.example.hobbitron.repository.HobbitRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class HobbitService {
+
+    private final HobbitRepository hobbitRepository;
+
+    public HobbitService(HobbitRepository hobbitRepository) {
+        this.hobbitRepository = hobbitRepository;
+    }
+
     public List<Hobbit> getAll() {
-        return List.of(
-                new Hobbit(1L, "Frodo", "Baggins"),
-                new Hobbit(2L, "Bilbo", "Baggins"),
-                new Hobbit(3L, "Penelope", "Tuk")
-        );
+        return hobbitRepository.findAll();
     }
 }
