261931509260730386f0de4f0215e1a3e99c8fd5
Lukasz <wyspianski.lukasz@gmail.com>
2021-10-12 10:20:51 +0000

    Initialize project:
    - Spring Web (endpoints)
    - Spring Data Jpa (easy db access)
    - H2 (in-memory db)
    - Lombok (no getters / setters )

---
+++ src/main/java/com/example/hobbitron/HobbitronApplication.java
@@ -0,0 +1,13 @@
+package com.example.hobbitron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbitronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HobbitronApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hobbitron/HobbitronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hobbitron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbitronApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
