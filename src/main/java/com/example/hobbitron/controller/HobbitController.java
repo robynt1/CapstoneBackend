625bb6539eb2faf995e33386af6622546fc794f0
lukasze <wyspianski.lukasz@gmail.com>
2021-10-13 11:45:23 +0200

    Populate db on application start (data.sql)

--- src/main/java/com/example/hobbitron/controller/HobbitController.java
+++ src/main/java/com/example/hobbitron/controller/HobbitController.java
@@ -2,6 +2,7 @@ package com.example.hobbitron.controller;
 
 import com.example.hobbitron.model.Hobbit;
 import com.example.hobbitron.service.HobbitService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- data.sql / schema.sql - Spring Boot will find it and use it
+INSERT INTO hobbit VALUES (1, 'Frodo','Baggins');
+INSERT INTO hobbit VALUES (2, 'Bilbo','Baggins');
+INSERT INTO hobbit VALUES (3, 'Penelope','Baggins');
