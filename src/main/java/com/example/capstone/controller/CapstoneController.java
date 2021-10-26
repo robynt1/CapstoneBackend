45793ee16dd00eb78555ae05511dc7b7e9172d6b
robynt1 <robyn6905@gmail.com>
2021-10-26 14:09:34 +0000

    telephone now working

--- src/main/java/com/example/capstone/controller/CapstoneController.java
+++ src/main/java/com/example/capstone/controller/CapstoneController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins="http://localhost:3000")
@@ -23,9 +24,21 @@ public class CapstoneController {
         return service.getAll();
     }
 
+    @GetMapping("/capstone/applicant")
+    Optional<Capstone> getApplicant(@RequestParam("id") Long id){ return service.getCustomer(id);}
+
     @PostMapping("/capstone")
     Capstone save(@RequestBody Capstone capstone) {
         return service.save(capstone);
+    }
+
+    @DeleteMapping("/capstone/delete")
+    void delete(@RequestParam("id") Long id){
+        service.deleteCustomer(id);
+    }
+    @PutMapping("/capstone/customerDetails")
+    void update(@RequestParam("id") Long id, @RequestParam("newTelephoneNumber") String newTelephoneNumber){
+        service.updateCustomer(id, newTelephoneNumber);
     }
 }
 
--- src/main/java/com/example/capstone/service/CapstoneService.java
+++ src/main/java/com/example/capstone/service/CapstoneService.java
@@ -3,11 +3,32 @@ package com.example.capstone.service;
 import com.example.capstone.model.Capstone;
 import com.example.capstone.repository.CapstoneRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CapstoneService {
+
+    public Optional<Capstone> getCustomer(Long id) {
+        return capstoneRepository.findById(id);
+    }
+
+    public  void deleteCustomer(Long id){
+        capstoneRepository.deleteById(id);
+    }
+
+    public  void updateCustomer(Long id, String newTelephoneNumber){
+        Optional<Capstone> customer = capstoneRepository.findById(id);
+        if(customer != null){
+            Capstone customerDetails = customer.get();
+            customerDetails.setTelNumber(newTelephoneNumber);
+            capstoneRepository.save(customerDetails);
+        }
+    }
 
     private final CapstoneRepository capstoneRepository;
 
